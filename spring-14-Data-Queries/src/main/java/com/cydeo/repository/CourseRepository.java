cda9590bd83c03667c0aa40f8160a0d3d4bd8e05
nataliiachobu <mebel2008@yahoo.com>
2022-10-28 08:38:22 -0700

    Queries/october 28

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -3,5 +3,17 @@ package com.cydeo.repository;
 import com.cydeo.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course,Long> {
+
+    //find all courses by category select  where category=
+    List<Course> findByCategory(String category);
+
+
+
+
+
+
+
 }
