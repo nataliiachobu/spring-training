ce2d39961f8ca0c0f9d5dac5c7a69fdcb16960fa
nataliiachobu <mebel2008@yahoo.com>
2022-10-28 12:42:55 -0700

    Queries/october 28/practice

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -3,10 +3,12 @@ package com.cydeo.repository;
 import com.cydeo.entity.Course;
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface CourseRepository extends JpaRepository<Course,Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 
     //find all courses by category select  where category=
     List<Course> findByCategory(String category);
@@ -16,15 +18,16 @@ public interface CourseRepository extends JpaRepository<Course,Long> {
     List<Course> findByCategoryOrderByName(String category);
 
     //Checks if a course with the supplied name exists. Return true if exists, false otherwise
-  boolean existsByName(String name);
+    boolean existsByName(String name);
+
     //Returns the count of courses for the given category
- int countByCategory(String category);
+    int countByCategory(String category);
 
     //Finds all the courses that start with the given course name string
- List<Course> findByCategoryStartingWith(String name);
+    List<Course> findByCategoryStartingWith(String name);
+
     //Find all courses by category and returns a stream
- List<Course> streamAllByCategory(String category);
-
-
-
+    List<Course> streamAllByCategory(String category);
+@Query("select c from Course c where c.category =:category and c.rating>:rating")
+    List<Course>retrieveAllByCategoryAndRatingGreaterThan(@Param("category") String category,@Param("rating")int rating);
 }
--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -3,15 +3,17 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Employee;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-
+//Derive query//
     //Display all employees with email address ""
     List<Employee> findByEmail(String email);
+
 
     //Display all employees with firstname "" and last name "",
     //also show all employees with an email address ""
@@ -33,4 +35,55 @@ public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
     Employee retrieveEmployeeDetail();
     @Query("SELECT e.salary FROM Employee e WHERE e.email='amcnee1@google.es'")
     Integer retrieveEmployeeSalary();
+    //not equal JPQL
+
+    @Query ("select e from Employee e where e.salary <> ?1")
+    List<Employee> retrieveEmployeeSalaryNotEqual(int salary);
+
+    //Like / contains/startsWiths/EndsWith
+    @Query ("select e from Employee e where e.firstName like ?1")
+    List<Employee> retrieveEmployeeFirstNameLike(String pattern);
+
+    //less than
+    @Query("select e from Employee e where e.salary< ?1")
+    List <Employee> retrieveEmployeeSalaryLessThan(int salary);
+
+    @Query("select e.firstName from Employee e where e.salary< ?1")
+    List <String> retrieveEmployeeSalaryLessThanNames(int salary);
+
+    @Query("select e from Employee e where e.salary> ?1")
+    List <Employee> retrieveEmployeeSalaryGreaterThan(int salary);
+
+//between
+    @Query("select e from Employee e where e.salary between ?1 and ?2")
+    List <Employee> retrieveEmployeeSalaryBetween(int salary1, int salary2);
+
+
+
+    //before
+    @Query ("select e from Employee e where e.hireDate > ?1")
+    List<Employee> retrieveEmployeeHireDateBefore(Local date);
+
+    //Null
+    @Query("select e from Employee e where e.email is null ")
+    List<Employee>  retrieveEmployeeEmailNull();
+
+    @Query("select e from Employee e where e.email is not null ")
+    List<Employee>  retrieveEmployeeEmailNotNull();
+
+    //sorting in asc order
+
+    @Query("select e from Employee e order by e.salary")
+    List<Employee>  retrieveEmployeeSalaryOrderAsc();
+
+    @Query("select e from Employee e order by e.salary desc ")// in descending way JPQL
+    List<Employee>  retrieveEmployeeSalaryOrderDesc();
+
+    //Native query Rule if you write not derive query you need to put @Query annotation
+    @Query(value ="select * from employees where salary =?1", nativeQuery = true)
+    List <Employee> retrieveEmployeeDetailBySalary(int salary);
+
+
+
+
 }
